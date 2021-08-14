24d07bceb508564ea9b5ac579486a025b6030f2e
RHT <rodrigohisa@gmail.com>
2021-08-14 12:35:12 -0300

    Implemented findByDepartment

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Arrays;
 import java.util.Date;
 
 import model.dao.DaoFactory;
@@ -10,14 +11,12 @@ import model.entities.Seller;
 public class Program {
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "Books");
 
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(sellerDao.findById(2));
-		System.out.println(obj);
-		System.out.println(seller);
+
+		Department department = new Department(2, null);
+		System.out.println(Arrays.toString(sellerDao.findByDepartment(department).toArray()));
 	}
 }
